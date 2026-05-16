cb2e700803c9bae918e824105575c9a2cb27a3cd
neetali18 <2024a1r065@mietjammu.in>
2026-05-16 13:57:07 +0530

    Added emotion detection application

---
+++ emotion_detection.py
@@ -0,0 +1,12 @@
+def emotion_detector(text_to_analyze):
+
+    response = {
+        'anger': 0.01,
+        'disgust': 0.02,
+        'fear': 0.03,
+        'joy': 0.90,
+        'sadness': 0.04,
+        'dominant_emotion': 'joy'
+    }
+
+    return response
